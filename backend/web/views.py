9773d5abb17cab8e1bd25bba65ab6120414f825b
Samir Das <samircd4@gmail.com>
2026-04-13 16:10:38 +0600

    fix(web): add queryset to StoreConfigurationView to resolve drf AssertionError

--- backend/web/views.py
+++ backend/web/views.py
@@ -14,6 +14,7 @@ class IsAdminOrReadOnly(BasePermission):
 from api.permissions import StaffHasActionPermission
 
 class StoreConfigurationView(generics.RetrieveUpdateAPIView):
+    queryset = StoreConfiguration.objects.all()
     serializer_class = StoreConfigurationSerializer
     permission_classes = [IsAuthenticated, StaffHasActionPermission]
 
